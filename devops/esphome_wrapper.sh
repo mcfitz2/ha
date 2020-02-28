6f973d30a3a14ad80dc7cf5d6f72773ec2ae7094
Micah Fitzgerald <mcfitz2@gmail.com>
2020-02-27 23:55:25 -0600

    added wrapper

---
+++ devops/esphome_wrapper.sh
@@ -0,0 +1,11 @@
+docker run -t --rm --net=host esphome-builder "$@" | tee output.log
+EC=$?
+ERR=$(cat output.log | tail -n2 |grep ERROR)
+rm output.log
+if [ "$EC" -gt 0 ]; then
+	exit $EC
+elif [ ! -z "$ERR" ]; then
+	exit 1
+else
+	exit 0
+fi
