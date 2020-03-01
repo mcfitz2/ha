d2fe71b202cfc2e84a9f842187249385ca689276
Micah Fitzgerald <mcfitz2@gmail.com>
2020-03-01 23:56:34 +0000

    ha and octoprint don't work. making this repo for esphome only

--- hass/run.sh
+++
@@ -1,6 +0,0 @@
-#mount s3 here
-echo ${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY} >  ${HOME}/.passwd-s3fs
-chmod 400 ${HOME}/.passwd-s3fs
-mkdir /config-aws
-s3fs $S3_PATH /config-aws -o passwd_file=${HOME}/.passwd-s3fs,url=https://s3.fr-par.scw.cloud
-/bin/entry.sh python3 -m homeassistant --config /config-aws
