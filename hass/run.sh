f1548739eeb2a547ff90ccf581f24c4672a7bbbd
Micah Fitzgerald <mcfitz2@gmail.com>
2020-03-01 14:00:28 -0600

    changing s3 mount program

--- hass/run.sh
+++ hass/run.sh
@@ -1,4 +1,5 @@
 #mount s3 here
+echo ${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY} >  ${HOME}/.passwd-s3fs
 
-yas3fs s3://$S3_PATH /config -f
+s3fs $S3_PATH /config 
 /bin/entry.sh python3 -m homeassistant --config /config
